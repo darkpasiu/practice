138cd96a834bfbc1f5e649dbe2e13d148c464e5f
Blazej Mirowski <darkpasiu@gmail.com>
2020-06-03 21:25:33 +0200

    add valgrind target
    Signed-off-by: Blazej Mirowski <darkpasiu@gmail.com>

--- apps/main.cpp
+++ apps/main.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 int main()
 {
-	cout << "dzien dobry ^^" << endl;
-	
-	return 0;
+    cout << "dzien dobry ^^" << endl;
+
+    // memory leak
+    char *x = new char[100];
+
+    return 0;
 } 
